b299e3efde61aedd26098156c23cc4468917cabc
KroqueJa <ji.krochmal@gmail.com>
2026-06-09 22:02:42 +0200

    Remove new build dir from repo

--- build-wsl/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/jikro/CLionProjects/qwc/tests
-# Build directory: /mnt/c/Users/jikro/CLionProjects/qwc/build-wsl/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/c/Users/jikro/CLionProjects/qwc/build-wsl/tests/qwc_tests[1]_include.cmake")
-subdirs("../thirdparty/googletest")
